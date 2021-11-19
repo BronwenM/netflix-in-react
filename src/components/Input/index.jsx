3cfa8b717baaaeb08bc5b4784995be6474b572c1
Bronwen <bronwen.marchant@gmail.com>
2021-11-19 18:39:47 -0500

    created input component w/ styles

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,7 +1,15 @@
 import './styles.css';
+import {BsChevronRight} from 'react-icons/bs'
 
 export const Input = (props) => {
+    
+    const {type, isRequired, name, placeholder, label, cta, ctaAction} = props;
+    
     return (
-        <input></input>
+        <div className="input-group">
+            {label && <label htmlFor={name}>{label}</label>}
+            <input type={type} required={isRequired} name={name} placeholder={placeholder}/>
+            {cta && <button onClick={ctaAction} className="cta">{cta} <BsChevronRight className="icon"/></button>}
+        </div>
     )
 }
--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -1,5 +1,6 @@
 import { Accordion } from '../../Accordion';
 import { Button } from '../../Button';
+import { Input } from '../../Input';
 import './styles.css';
 
 export const HomePage = () => {
@@ -8,6 +9,7 @@ export const HomePage = () => {
             <Button text="Primary" type="primary" isDisabled={false} action={() => alert("Button Clicked")}></Button>
             <Button text="Secondary" type="secondary" isDisabled={false} action={() => alert("Button Clicked")}></Button>
             <Accordion title="Test" body="hello world"/>
+            <Input type="email" name="email" isRequired={true} placeholder="Enter email here..." cta="Get Started"/>
         </div>
     )
 }
