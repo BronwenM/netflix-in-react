b522842cf072c1187b16f2ad28e281c0c12f23bf
Bronwen <bronwen.marchant@gmail.com>
2021-11-19 23:46:53 -0500

    added missing content to accordion

--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -74,8 +74,10 @@ export const HomePage = () => {
     
     You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere."/>
                     <Accordion title="How do I cancel?" body="hello world"/>
-                    <Accordion title="Test" body="hello world"/>
-                    <Accordion title="Test" body="hello world"/>
+                    <Accordion title="What can I watch on Netflix?" body="Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want."/>
+                    <Accordion title="Is Netflix good for kids?" body="The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space.
+
+Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see."/>
                     <br></br>
                     <div>
                         <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
